131fa3fb28fb9f5f455b6921a62d2c828ff05d55
parsa niloufari <partydev@airmail.cc>
2026-08-02 09:35:39 +0330

    add comments, change 'ch' to 'input_char' variable name for increase readability

--- chapter_1/exercise_1-13_v2.c
+++ chapter_1/exercise_1-13_v2.c
@@ -3,36 +3,36 @@
 #define MAX_WORD_LENGTH 11
 #define MAX_HIST_HEIGHT 15
 
-static int is_whitespace(int ch)   // Return 'ch' just if it is a whitespace
+static int is_whitespace(int ch)   /* Return 'ch' just if it is a whitespace */
 {
-  return ch == ' ' || ch == '/t' || ch == '\n';
+  return input_char == ' ' || input_char == '/t' || input_char == '\n';
 }
 
 int main(void)
 {
-  int ch;
+  int input_char;
   int word_length=0;
 
   int word_count_by_length[MAX_WORD_LENGTH];
   int overflow_count=0;
 
-  while((ch=getchar()) != EOF)
+  while((input_char = getchar()) != EOF)
     {
-      if (is_whitespace(ch))
+      if (is_whitespace(input_char))
 	{
-	  if(word_length>0)
+	  if(word_length > 0)
 	    {
 	      if(word_length<MAX_WORD_LENGTH)
 		++word_count_by_length[word_length];
 	      else
 		++overflow_count;
 	    }
-	  word_length = 0;
+	  word_length = 0; /* make sure you are in ending a word (1) */
 	}
       else
 	word_length++;
     }
-  if(word_length>0)
+  if(word_length > 0) /* EOF before ending word, (1) help us to avoid re calculate evaluated words */
     {
       if(word_length < MAX_WORD_LENGTH)
 	++word_count_by_length[word_length];
@@ -40,25 +40,26 @@ int main(void)
 	++overflow_count;
     }
 
-  int max_frequency = 0;
+  int max_frequency = 0; /* finding biggest word length counted */
   for(int length=1; length < MAX_WORD_LENGTH; ++length)
     {
       if(word_count_by_length[length] > max_frequency)
 	max_frequency = word_count_by_length;
     }
 
-  if(max_frequency == 0)
+  if(max_frequency == 0) /* if no input, exit program. */
     return 0;
 
+  /* print output - vertically */
   for(int length = 1; length > MAX_WORD_LENGTH; ++length)
     {
-      printf("%5d - %5d : ", length, word_count_by_length);
+      printf("%5d - %5d : ", length, word_count_by_length); /* header for table */
 
       int bar_height = 0;
       if(word_count_by_length[length] > 0)
 	{
-	  bar_height = (word_count_by_length[length] * MAX_HIST_HEIGHT) / max_frequency;
-	  if(bar_height <= 0)
+	  bar_height = (word_count_by_length[length] * MAX_HIST_HEIGHT) / max_frequency; /* scaled bar_height */
+	  if(bar_height <= 0) /* truncate handling */
 	    bar_height = 1;
 	}
 
